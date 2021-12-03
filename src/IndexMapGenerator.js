ebd2f9d7f78fda0d471221193544df341dab8d98
Yogesh Kotadiya <hi@yogeshkotadiya.com>
2021-12-03 10:13:31 +0530

    fix: set default value for sources

--- src/IndexMapGenerator.js
+++ src/IndexMapGenerator.js
@@ -29,7 +29,7 @@ export default class SiteMapIndexGenerator {
     }
 
     generateSiteMapUrlElements({
-        sources,
+        sources = [],
         siteUrl,
         pathPrefix,
         resourcesOutput,
